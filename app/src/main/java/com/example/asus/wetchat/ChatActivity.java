1f1a74b447305fc88330c8f1bccce2de48496912
Bat_Cat <2499901339@qq.com>
2017-05-18 16:30:16 +0800

    第一次commit

---
+++ app/src/main/java/com/example/asus/wetchat/ChatActivity.java
@@ -0,0 +1,76 @@
+package com.example.asus.wetchat;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatActivity extends AppCompatActivity {
+    private List<Msg> msgList = new ArrayList<>();
+    private EditText inputText;
+    private Button send;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initMsgs();
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgRecyclerView = (RecyclerView) findViewById(R.id.msg_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+        send.setOnClickListener(v -> {
+            String content = inputText.getText().toString();
+            if(!"".equals(content)){
+                Msg msg = new Msg(content, Msg.TYPE_SEND);
+                msgList.add(msg);
+                adapter.notifyItemChanged(msgList.size() - 1);
+                msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                inputText.setText("");
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.information:
+                Toast.makeText(this, "You clicked information", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+    private void initMsgs(){
+        Msg msg1 = new Msg("Hello.My friend", Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("Hello.Who is that?", Msg.TYPE_SEND);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("This is Tom.\n"+"Nice talking to you." ,Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
+    }
+}
